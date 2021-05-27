bba802d4b18a9578ab7db2d506e9cd3c84c04b60
hieppham02 <83917749+hieppham02@users.noreply.github.com>
2021-05-27 14:52:32 +0700

    Update app.js

--- Exercises_SS5/app.js
+++ Exercises_SS5/app.js
@@ -5,57 +5,48 @@
 // let str = prompt("Enter a string");
 // console.log(startWith(str));
 //Bai 2:
-// function findMaxDiff (arr,n){
-//     let b = [];
-//     if (n<=1){
-//         return 0;
-//     }else{
-//         for (let i=0; i<n; i++){
-//         b[i] = a[i]-a[i+1];
-//         }
-//     let max = b[0];
-//     for (let i=0; i<n; i++){
-//         if (b[i]>max){
-//             max=b[i];
-//         }
+// function findMaxDiff (arr){
+//     if( arr.length === 0){
+//       return 0;
+//     }else if(arr.length === 1){
+//       return 0;
+//     }else {
+//      let  max = Math.abs(arr[1] - arr[0]);
+//          for(let i = 1; i < arr.length; i++)
+//              if(Math.abs(arr[i+1]-arr[i]) > max)
+//                  max = Math.abs(arr[i+1] - arr[i]);
+//          return max;
 //     }
-//     return max;
-//     }  
-// }
+//    }
 // let a =  [1, -10, 5, 18, -9, 5];
-// let n = a.length;
-// console.log(findMaxDiff(a,n));
+// console.log(findMaxDiff(a));
+
 //Bai 3:
-// function first (arr,n,length){
+// function first (arr,n){
 //     if (n<=0){
 //         return [];
-//     } else if (n>length){
+//     } else if (n>arr.length){
 //         return arr;
-//     }else if (n<=length) {
+//     }else if (n<=arr.length) {
 //         return arr.splice(0,n);
 // }
 // }
 // let n = Number(prompt("Enter number of element "));
 // let a = [1,5,62,-13,3,5];
-// let length = a.length;
 // console.log(...first(a,n,length));
 //Bai 4:
 // function checkInt (number1,number2){
 //     if (((number1 < 0) && (number2>0)) || ((number2<0) && (number1>0))){
-//         return 1;   
+//         alert("TRUE")   
 //     } else if ((number1==0) || (number2==0)) {
-//         return 0;
+//         alert("FALSE");
 //     } else {
-//         return 0;
+//         alert("FALSE");
 //     }
 // }
 // let a = Number(prompt("Enter number a:"));
 // let b = Number(prompt("Enter number b:"));
-// if (checkInt(a,b)==1){
-//     alert("TRUE");
-// } else {
-//     alert("FALSE");
-// }
+// checkInt(a,b);
 //Bai 5:
 // function JavaScript (n){
 //     let x = Math.abs(n-13);
@@ -68,19 +59,32 @@
 // let inputNumber = Number(prompt("Enter a number"));
 // alert(JavaScript(inputNumber));
 //Bai 6:
-// function findMaxDiff2 (arr,n){
-//     let maxDiff = arr[0];
-//     let b =[];
-//     for (let i=0; i<n; i++){
-//         b[i]=arr[i+1]-maxDiff;
+//O(n^2)
+// function findMaxDiff2( arr){  
+// let size = arr.length;   
+// let maxDiff = arr[1] - arr[0];
+//   for (let i = 0; i < size; i++)
+//   {
+//     for (let j = i+1; j < size; j++)
+//     {    
+//       if (arr[j] - arr[i] > maxDiff)
+//         maxDiff = arr[j] - arr[i];
 //     }
-//     let max = b[0];
-//     for (let i=0; i<n; i++){
-//         if (max>b[i]){
-//             max=b[i];
-//         }
-//     }
-//     return max;
+//   }        
+//   return maxDiff;
 // }
-// let n = [1, 2, 3, 8, 9].length;
-// console.log(findMaxDiff2([1, 2, 3, 8, 9],n));
+//O(n)
+// function findMaxDiff2(arr){
+//         let size = arr.length;
+//         let maxDiff = arr[1] - arr[0];
+//         let minElement = arr[0];
+//         for (let i = 0; i<size; i++) {
+//             if (arr[i] - minElement > maxDiff)
+//                 maxDiff = arr[i] - minElement;
+//             if (arr[i] < minElement)
+//                 minElement = arr[i];
+//         }
+//         return maxDiff;
+// }
+// let arr = [1,5,3,8,9];
+// console.log(findMaxDiff2(arr));       
